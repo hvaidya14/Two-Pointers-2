698b88994235a9a431d3fe4f30ec6c2d84f187b7
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-01-14 22:35:44 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -30,3 +30,50 @@ class Solution {
 
     }
 }
+
+
+// merge sorted array
+//TIME COMPLEXITY:O(M+M)
+//SPACE COMPLEXITY:O(1)
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int p3 = m+n-1;
+        int p2= nums2.length-1;
+        int p1= m-1;
+
+
+        if (m ==0){
+            while (p2>=0) {
+                nums1[p3] = nums2[p2];
+                p2--;
+                p3--;
+            }
+
+        }
+        while(p2>=0 && p1 >=0) {
+            if (nums2[p2] > nums1[p1]) {
+                nums1[p3] = nums2[p2];
+                p3--;
+                p2--;
+            } else if(nums1[p1] > nums2[p2]) {
+                nums1[p3] = nums1[p1];
+                p3--;
+                p1--;
+                System.out.println(p3);
+            } else{
+                nums1[p3] = nums2[p2];
+                p2--;
+                p3--;
+            }
+        }
+
+        while (p2>=0) {
+            System.out.println(p3);
+            System.out.println(p3);
+            nums1[p3] = nums2[p2];
+            p3--;
+            p2--;
+        }
+    }
+}
