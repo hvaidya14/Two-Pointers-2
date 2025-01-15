e0514d2319148a81ee8e7569286b7e08351ec3e9
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-01-14 22:04:16 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -5,3 +5,28 @@
 // Three line explanation of solution in plain english
 
 // Your code here along with comments explaining your approach
+
+//TIME COMPLEXITY:O(N)
+//SPACE COMPLEXITY:O(1)
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+
+        int col = matrix[0].length-1;
+        int row = 0;
+
+
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                col--;
+            } else if (target > matrix[row][col]) {
+                row++;
+            }
+        }
+        return false;
+
+    }
+}
