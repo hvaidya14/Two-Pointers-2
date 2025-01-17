8721c389f7b6c13fa6dc3a3587bb09e17a4eff14
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-01-16 18:11:04 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -77,3 +77,34 @@ class Solution {
         }
     }
 }
+
+
+// remove duplicates
+// SPACE COMPLEXITY:O(1)
+// TC:O(N)
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        int count = 1;
+        int j=1;
+
+        for (int i=1;i<nums.length;i++) {
+            if (nums[i] == nums[i-1]) {
+                count++;
+                if (count <= 2) {
+                    nums[j] = nums[i];
+                    j++;
+                }
+            } else {
+
+                nums[j] = nums[i];
+                j++;
+
+                count=1;
+            }
+        }
+        return j;
+        
+    }
+}
